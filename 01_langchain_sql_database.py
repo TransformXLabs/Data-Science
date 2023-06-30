69b51dcda3c11222242e12061f82c89b137421f9
Matt Dancho <mdancho@gmail.com>
2023-06-30 13:51:33 -0400

    taking a break from langchain

--- 01_langchain_sql_database.py
+++ 01_langchain_sql_database.py
@@ -46,6 +46,8 @@ llm = OpenAI(
 
 db = SQLDatabase(engine=sql_engine, metadata=metadata)
 
+# Default Response
+
 sql_chain = SQLDatabaseChain.from_llm(
     llm                       = llm, 
     db                        = db, 
@@ -56,7 +58,7 @@ sql_chain = SQLDatabaseChain.from_llm(
 
 sql_chain.run("What are the top 5 most popular bikes?")
 
-# Using intermediate steps
+# Response using intermediate steps
 
 sql_chain_with_steps = SQLDatabaseChain.from_llm(
     llm                       = llm, 
@@ -74,44 +76,89 @@ result_chain.keys()
 
 result_chain["intermediate_steps"][1]
 
-# 3.0 RUN THE AGENT USING A PROMPT THAT GENERATES SQL CODE
-
-prompt = """
-What are the top 5 most popular bikes?
-"""
-
-ret = zero_shot_agent.run(prompt)
-
-ret
-
-prompt = """
-What are the top 5 most popular bikes?
-
-Use the list to create a table with the top 5 most popular bikes and both the quantity sold and the sales total for each bike.
-"""
-
-
-ret = zero_shot_agent.run(prompt)
-
-ret
-
-# USING THE SQL TO PRODUCE A TABLE
+# 3.0 USING THE SQL TO PRODUCE A TABLE
 
 pd.read_sql_query(sql.text("SELECT * FROM bikes"), conn)
-
 
 sql_text = 'SELECT model, COUNT(*) AS popularity FROM bikes INNER JOIN orderlines ON bikes."bike.id" = orderlines."product.id" WHERE orderlines."product.id" IS NOT NULL GROUP BY model ORDER BY popularity DESC LIMIT 5;'
 
 pd.read_sql_query(sql.text(sql_text), conn)
 
+# 4.0 RUN THE AGENT USING A PROMPT THAT GENERATES SQL CODE
+
+# * Example 1: What are the top 5 most popular bikes?
+
+prompt = """
+What are the top 5 most popular bikes?
+"""
+
+ret = sql_chain_with_steps(prompt)
+
+ret['result']
+
+ret['intermediate_steps'][1]
+
+pd.read_sql_query(sql.text(ret['intermediate_steps'][1]), conn)
+
+# * Example 2: total sales by unit and value?
+
+prompt = """
+What are the total sales in units and by total value (unit price times quantity)?
+"""
+
+ret = sql_chain_with_steps(prompt)
+
+ret
+
+ret['result']
+
+ret['intermediate_steps'][1]
+
+pd.read_sql_query(sql.text(ret['intermediate_steps'][1]), conn)
+
+# * Example 3: total sales for each bike shop?
+
+prompt = """
+What are the total sales in units and by total value (unit price times quantity) by bikeshop for for the top 10 bike shops?
+"""
+
+ret = sql_chain_with_steps(prompt)
+
+ret
+
+ret['result']
+
+ret['intermediate_steps'][1]
+
+pd.read_sql_query(sql.text(ret['intermediate_steps'][1]), conn)
+
+# * Example 4: Sales value (unit price times quantity) by order date for the top 5 bike shops?
+
+prompt = """
+Multiply the unit price by the quantity of bikes sold for each order.
+"""
+
+ret = sql_chain_with_steps(prompt)
+
+ret
+
+ret['result']
+
+ret['intermediate_steps'][1]
+
+pd.read_sql_query(sql.text(ret['intermediate_steps'][1]), conn)
 
 
 
-# MAKE AN AGENT TO RUN THE SQL CHAIN
+
+
+
+
+# 3.0 CREATE AN AGENT TO RUN THE SQL CHAIN
 
 sql_tool = Tool(
     name        = "Bike Orders Database",
-    func        = sql_chain.run,
+    func        = sql_chain_with_steps,
     description = "Query bikes orders database.",
 )
 
@@ -127,5 +174,8 @@ zero_shot_agent = initialize_agent(
     tools          = tools,
     llm            = llm,
     verbose        = True,
-    max_iterations = 5,
+    
+    # Important to specify a max number of iterations or could run for a long time
+    max_iterations = 5,  
 )
+
