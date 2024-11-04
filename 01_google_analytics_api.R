62b3193cb7054bea100c4e67de4213ff2542e308
Bayan Alsaghier <alsaghierbayan@gmail.com>
2024-11-04 17:38:49 +0300

    L31

---
+++ 01_google_analytics_api.R
@@ -0,0 +1,154 @@
+# LEARNING LAB 31: Google Analytics, Facebook Prophet, & Shiny
+# OBJECTIVE: Forecast User Visits by Page Path
+# 01 - GOOGLE ANALYTICS API ----
+
+
+# 1.0 LIBRARIES ----
+
+# API - ACCESS GA
+library(googleAnalyticsR)
+
+# DATABASE - SAVE GA
+library(DBI)
+library(RSQLite)
+
+#Visualization
+library(plotly)
+
+# Core
+library(tidyverse)
+library(tidyquant)
+library(lubridate)
+
+
+
+# 2.0 GETTING DATA ----
+# - Resource: https://code.markedmondson.me/googleAnalyticsR/
+
+# Authentication
+ga_auth()
+
+# Setup (for reproducibility)
+end_date     <- ymd("2020-03-24")
+start_date   <- end_date - 30
+
+# Access GA's API
+sessions_tbl = google_analytics(
+    viewId = config::get("googleanalytics-gtm-view-1"),	# replace this with your view ID
+    
+    # Date Range
+    date_range = c(start_date, end_date),
+    
+    # Measuring
+    metrics = "sessions",
+    dimensions = "date"
+) %>% as_tibble()
+
+sessions_tbl
+
+
+
+# 3.0 DIMENSIONS & METRICS -----
+# - Resource: https://ga-dev-tools.appspot.com/dimensions-metrics-explorer/
+
+ga_sessions_users_transactions_tbl = google_analytics(
+    viewId = config::get("googleanalytics-gtm-view-1"), # replace this with your view ID
+    date_range = c(today() - 30, today()),
+    
+    # Metrics (Values)
+    metrics = c("sessions", "users", "transactions"),
+    
+    # Dimensions (Segments)
+    dimensions = c("date"),
+    
+    # Anti-sample & Max - Get the full data with multiple API calls 
+    anti_sample = TRUE, 
+    max = -1
+) %>%
+    as_tibble()
+
+ga_sessions_users_transactions_tbl
+
+
+
+# 4.0 VISUALIZATION ----
+# - dplyr, ggplot2 + plotly (101)
+
+ga_sessions_users_transactions_tidy_tbl <- ga_sessions_users_transactions_tbl %>%
+    pivot_longer(cols = sessions:transactions) %>%
+    arrange(date) %>%
+    group_by(name) %>%
+    mutate(growth = CUMULATIVE_SUM(value)) %>%
+    ungroup() %>%
+    arrange(name, date)
+
+ga_sessions_users_transactions_tidy_tbl
+
+# 4.1 Visualize Counts ----
+g <- ga_sessions_users_transactions_tidy_tbl %>%
+    ggplot(aes(date, value, color = name)) +
+    geom_line(size = 1) +
+    facet_wrap(~ name, scales = "free_y", ncol = 1) +
+    theme_tq() +
+    scale_color_tq() +
+    labs(title = "Google Analytics - Sessions, Transactions, Users over Time")
+
+g
+
+ggplotly(g)
+
+
+# 4.2 Visualize Growth ----
+g <- ga_sessions_users_transactions_tidy_tbl %>%
+    ggplot(aes(date, growth, color = name)) +
+    geom_line(size = 1) +
+    facet_wrap(~ name, scales = "free_y", ncol = 1) +
+    theme_tq() +
+    scale_color_tq() +
+    labs(title = "Google Analytics - Sessions, Transactions, Users over Time")
+
+g
+
+ggplotly(g)
+
+
+
+# 5.0 DATABASE ----
+# - Learning Labs 21, 22, & 23 (SQL Series Labs)
+
+# 5.1 Access Page Path Data ----
+ga_page_views_tbl = google_analytics(
+    viewId = config::get("googleanalytics-gtm-view-1"), # replace this with your view ID
+    date_range = c("2019-09-01", "2020-03-16"),
+    
+    # Metrics (Values)
+    metrics = c("pageViews", "organicSearches"),
+    
+    # Dimensions (Segments)
+    dimensions = c("pagePath", "date"),
+    
+    # Anti-sample & Max - Get the full data with multiple API calls 
+    anti_sample = TRUE, 
+    max = -1
+) %>%
+    as_tibble()
+
+ga_page_views_tbl
+
+# 5.2 Write Data to SQLite ----
+
+con <- dbConnect(SQLite(), "data/google_analytics_2.sqlite")
+
+dbListTables(con)
+
+dbWriteTable(con, "ga_page_views", ga_page_views_tbl)
+
+dbListTables(con)
+
+# 5.3 Dplyr Database Backend ----
+# - Lab 21, 22, & 23 (SQL Series) 
+
+tbl(con, "ga_page_views")
+
+tbl(con, "ga_page_views") %>% collect()
+
---
+++ 02_prophet_model.R
@@ -0,0 +1,184 @@
+# LEARNING LAB 31: Google Analytics, Facebook Prophet, & Shiny
+# OBJECTIVE: Forecast User Visits by Page Path
+# 02 - FACEBOOK PROPHET ALGORITHM ----
+
+
+# 1.0 LIBRARIES ----
+
+# Database
+library(DBI)
+library(RSQLite)
+
+# Time Series Forecasting
+library(tsibble)
+library(fable)
+# remotes::install_github("mitchelloharawild/fable.prophet")
+library(fable.prophet)
+
+#Visualization
+library(plotly)
+
+# Core
+library(tidyverse)
+library(tidyquant)
+library(lubridate)
+
+# 2.0 READ SQL ----
+
+con <- dbConnect(SQLite(), "data/google_analytics.sqlite")
+
+DBI::dbListTables(con)
+
+ga_page_views_tbl <- tbl(con, "ga_page_views") %>% collect()
+
+dbDisconnect(con)
+
+# Issue - SQLite doesn't have a "Date" class (gets converted to numeric)
+# - Don't freak out! Everything will be OK.
+
+ga_page_views_tbl <- ga_page_views_tbl %>%
+    mutate(date = as_date(date))
+
+
+# 3.0 DATA PREP ----
+# - dplyr (101)
+
+ga_page_views_tbl %>% distinct(pagePath)
+
+# 3.1 Top 10 Pages by Total Search & Organic Search ----
+
+ga_page_views_summary_tbl <- ga_page_views_tbl %>%
+    group_by(pagePath) %>%
+    summarize(
+        pageViews = SUM(pageViews),
+        organicSearches = SUM(organicSearches)
+    ) 
+
+top_10_page_views_summary_tbl <- ga_page_views_summary_tbl %>%
+    arrange(desc(pageViews)) %>%
+    slice(1:10)
+
+top_10_page_views_summary_tbl
+
+top_10_organic_searches_summary_tbl <- ga_page_views_summary_tbl %>%
+    arrange(desc(organicSearches)) %>%
+    slice(1:10)
+
+top_10_organic_searches_summary_tbl
+
+
+# 3.2 Pare Down Page Path Options ----
+
+ga_page_views_tidy_tbl <- ga_page_views_tbl %>%
+    filter(pagePath %in% c(
+        top_10_organic_searches_summary_tbl %>% pull(pagePath),
+        top_10_page_views_summary_tbl %>% pull(pagePath)
+    )) %>%
+    pivot_longer(cols = pageViews:organicSearches, names_to = "search_type") %>%
+    select(date, everything()) %>%
+    arrange(date, pagePath, search_type)
+
+ga_page_views_tidy_tbl
+
+
+
+# 4.0 PROPHET & FABLE ----
+
+# 4.1 Setup ---- 
+page_path_options   <- ga_page_views_tidy_tbl %>% pull(pagePath) %>% unique()
+page_path_selection <- page_path_options[1]
+forecast_horizon    <- 30
+
+# 4.2 Convert to TSibble ----
+ga_page_views_filtered_tsbl <- ga_page_views_tidy_tbl %>%
+    filter(pagePath == page_path_selection) %>%
+    as_tsibble(
+        key   = c(pagePath, search_type), 
+        index = date
+    )
+
+# 4.3 Prophet Model ----
+page_views_mable <- ga_page_views_filtered_tsbl %>%
+    model(
+        PROPHET = prophet(log(value) ~ season(period = "week"))
+    )
+
+page_views_mable
+
+# 4.4 Prophet Forecast ----
+g <- page_views_mable %>%
+    forecast(h = forecast_horizon) %>%
+    
+    autoplot(ga_page_views_filtered_tsbl) +
+    theme_tq() +
+    scale_color_tq() +
+    labs(
+        title = "Prophet Model: With Weekly Seasonality",
+        x = "", y = "Traffic (Users)"
+    ) 
+
+g
+
+# Wrong plot!! (Don't freak out)
+ggplotly(g)
+
+
+# 4.5 Plotly Visualization ----
+
+train_tbl <- ga_page_views_filtered_tsbl %>% as_tibble()
+
+page_views_fable <- page_views_mable %>% forecast(h = forecast_horizon)
+
+# Hilo Object
+hl <- page_views_fable %>%
+    mutate(interval_95 = hilo(value, 95)) %>%
+    slice(1) %>%
+    pull(interval_95) 
+
+hl
+
+# Purrr Plucking!
+future_tbl <- page_views_fable %>%
+    mutate(
+        lo_95 = quantile(value, 0.025),
+        hi_95 = quantile(value, 0.975)
+    ) %>%
+    as_tibble()
+
+# Plotly DataViz
+g <- bind_rows(
+    train_tbl %>% mutate(.mean = value) %>% select(-value), 
+    future_tbl
+) %>%
+    mutate(.model = ifelse(is.na(.model), "actual", .model) %>% as_factor()) %>%
+    mutate(search_type = as.factor(search_type) %>% fct_rev()) %>%
+    ggplot(aes(date, .mean, color = .model)) +
+    geom_ribbon(aes(ymin = lo_95, ymax = hi_95), alpha = 0.5, color = "white", fill = "dodgerblue") +
+    geom_line() +
+    facet_wrap(~ pagePath + search_type, ncol = 1, scales = "free_y") +
+    theme_tq() +
+    scale_color_tq() +
+    labs(x = "", y = "")
+
+ggplotly(g)
+
+
+# 5.0 SUMMARY - WHAT WE DIDN'T LEARN ----
+# - Congrats - We've made a forecast!
+
+# For time series Accuracy is EVERYTHING
+# - Accuracy saves your company a ton of money
+
+# We don't know:
+# 1. How good it is (Time Series Cross Validation)
+# 2. How to improve the model (Feature Engineering & Preprocessing)
+# 3. If there are better models 
+#   - Univariate (ARIMA, ETS, STL, TSLM)
+#   - Machine Learning (TSML-GLM, TSML-Xgboost, TSML-KNN, & 100 others)
+#   - Deep Learning - TSDL-RNN's
+# 4. How to ensemble multiple models (Competition Winning Strategy #1)
+# 5. How to model errors (Competition Winning Strategy #2)
+# 6. Feature-based model selection (Competition Winning Strategy #3)
+# 7. How to use Deep Learning (Competition Winning Strategy #4)
+
+# Advanced Time Series Forecasting (Coming Soon!) 😃😃😃
